34eaf11e28094c6633dfac4909cbef134ebc056c
devil404 <devilbhoir03@gmail.com>
2022-04-13 01:57:51 +0530

    adding modal to page

--- frontend/construction/js/script.js
+++ frontend/construction/js/script.js
@@ -1,8 +1,4 @@
  let navbar = document.querySelector('.header .navbar');
-//  let searchForm = document.querySelector('.header .search-form');
-//  let loginForm = document.querySelector('.header .login-form');
-//  let contactInfo = document.querySelector('.contact-info');
-
  document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
  };
